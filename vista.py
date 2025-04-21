93647224a22aacbce887f89a63eec8c197fc5db6
EdwinDavid848 <147579883+EdwinDavid848@users.noreply.github.com>
2025-04-20 21:50:45 -0500

    Update vista.py

--- vista.py
+++ vista.py
@@ -31,7 +31,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:8080",
-        "http://127.0.0.1:8080"
+        "http://127.0.0.1:8080",
+        "https://arqitectura-limpia-production.up.railway.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
